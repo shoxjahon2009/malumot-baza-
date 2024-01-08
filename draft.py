85e7a355b6ea20401ff311fcfbad8d83ae3a840a
Пользователь <shohjaxondehqonboyev59@gmail.com>
2024-01-08 21:29:55 +0500

    first commit

---
+++ draft.py
@@ -0,0 +1,6 @@
+name = ["shox","ali","vali","azamat"]
+for x in name:
+    if x == "ali":
+        name.remove("ali")
+
+print(name)
---
+++ main.py
@@ -0,0 +1,261 @@
+# # print("Hello world!") # Matinni konsolga chiqarish
+# # print(2 + 3) # Koshish
+# # print(2 - 3) # Ayirish
+# # print(-2 - 3) # Ayirish
+# # print(-2 * 3) # Kopaytirish
+# # print(2 * 3) # Kopaytirish
+# # print(3 / 2) # Toza Bolish
+# # print(3 // 2) # Bolish(Butun qisim qoladi)
+# # print(3 % 2) # Qoldiq
+# #
+# # number = 2
+# # person = "Iskandar"
+# # print(person)
+#
+# #Malumotlar turlari 3 xil bo'ladi
+# # 1 - Integer bular sonlar -cheksizlikdan + cheksizlikkacha
+# # 2 - String bular qo'shtirnoqni ichiga yoziladi
+# # 3 - Booling -> True, False
+#
+# # print(12) # Integer
+# # print("Ali") # String
+# # print(True, False) # Booling
+# #
+# # # Taqqoslash
+# # print(2 > 3)
+# # print(3 < 2)
+# # print(5 < 6)
+# # print(5 > 4)
+# # print(5 == 5)
+# # print(5 >= 5)
+# # print(5 >= 3)
+# # print(5 != 3)
+# # print(3 != 3)
+#
+#
+# # # name = input("Введите имя: ")
+# # # age = input("Введите возрасть: ")
+# # # print("Привет " + name)
+# # # print("Ваш возрасть " + age)
+# #
+# #
+# # с = "Name" + " Mirabbos"
+# # print(с)
+# #
+# #
+# # # x = input("x: ")
+# # # y = input("y: ")
+# # # print("Javob " +  x + y)
+# #
+# #
+# #
+# # #
+# # #
+# # # x = int(input("X: "))
+# # # y = int(input("Y: "))
+# # # print("Sizning" + " Javobingiz", x + y)
+# #
+# # a = "Kometa "
+# # b = 10
+# # d = a * b
+# # print(d)
+# #
+# #
+# # print(2 ** 5)
+# #
+# # x = int(input("Son: "))
+# # y = int(input("Daraja: "))
+# # print(x ** y)
+#
+#
+# # x = int(input("Bo'linuvchi: "))
+# # y = int(input("Bo'luvchi: "))
+# # print("Qoldiq: ", x % y)
+#
+# # x = int(input("Tomon: "))
+# #
+# # print("Yuzi", x ** 2)
+# #
+# # print("Perimetr: ", 4 * x)
+#
+#
+# # x = int(input("Diametr: ")) #r = d // 2
+# # print("Yuzi", 3.14 * (x // 2) ** 2) #S= п * R^2
+#
+#
+# # x = int(input("Katet a: "))
+# # y = int(input("Katet b: "))
+# # print("Gipatenuza: ", ((x ** 2) + (y ** 2)) ** (1//2))
+# #
+# #
+# # a = 4 ** (1//2)
+# # print(a)
+#
+# #
+# # name = "Ali"
+# # age = 'Ordak'
+# # adress = """Mira 4-2"""
+# # adress1 = '''Olmaliq shahar'''
+# # print(name, age, adress, adress1)
+#
+#
+# # person = "name: Firdavs \nage: 12 \nphone_number: \t+99894 411 11 11"
+# # print(person)
+#
+# # message = "Hello world!"
+# # print(f"{message}")
+#
+# # a = 12
+# # print(f"Men {a} yoshdaman")
+#
+# # ch = 6
+# # sh = 7
+# # print(f"Gipatenuzasi {((ch ** 2) + (sh ** 2)) ** (1/2)}ga teng!")
+#
+# #
+# # name = input('Ism: ')
+# # surname = input('Familiya: ')
+# # age = int(input('Yosh: '))
+# #
+# # if name == "Kamoliddin" and surname == "Umarov" or age == 24:
+# #     print("Здраствуйте")
+# #
+# # else:
+# #     print("Ты не админстратор!")
+# #
+# #
+# # name = "Kamoliddin"
+# # print(f"Mening ismim {name}!!!")
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # name = ["shox","ali","vali","azamat"]
+# # for x in name:
+# #     if x == "ali":
+# #         name.remove("ali")
+# #
+# # print(name)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# #
+# # names = [ name = "shox","ali","vali","azamat"]
+#
+#
+# # a = int(input("son:"))
+# # num = [1,2,3,4,5]
+# # if a in num:
+# #     print("BOER SHU RAQAM")
+# # else:
+# #     print("yoq bu raqam")
+#
+#
+# # cars = ["matiz","cobalt","malibu","gentra"]
+# # for car in cars:
+# #     print(car)
+# #
+# #     if car == "cobalt":
+# #         print(f"{car}: qimmat ")
+# #
+# #     elif car == "gentra":
+# #         print(f"{car}: qimmat")
+# #     else:
+# #         print(f"{car}: bimiman")
+#
+# # numbers = [1,2,3,4,5]
+# # reasult = 1
+# # for num in numbers:
+# #     reasult = reasult * num
+# # print(reasult)
+#
+# # name = ["shox","ali","vali","azamat"]
+#
+# # numbers = [1, 2, 3, 4, 5]
+# # resault = 1
+# # for num in numbers:
+# #     resault = resault * num
+# # print(resault)
+#
+# # name = [1,2,3,4,5]
+# # name.append(name)
+# # print(name)
+#
+# # numbers = [1,2,3,4,5,6,7,8,9,10]
+# # numbers.append(11)
+# # print(numbers)
+#
+# # numbers = []
+# # for numbers in range(1,16):
+# #     print(numbers)
+#
+# # person = {
+# #     "human": {
+# #         "name": "shohjahon",
+# #         "age": 14,
+# #         "happy birthday": "mart",
+# #         "phone number": "+998936178627"
+# #     }
+# # }
+# # for values in person.values():
+# #     print()
+#
+# # n = [1,2,3,4,5]
+# # a = n[0],n[2] = n[2],n[0]
+# # print(n)
+#
+#
+# # a = {1,2,3}
+# # b = {1,4,5}
+# # print(b-a)
+# # print(a-b)
+#
+#
+# car = {
+#     "car":"ferrari",
+#     "mod":"lambo"
+# }
+# a = car["car"] = "motoa"
+# print(car)
+#
+# del car["car"]
+# print(car)
+
+
+# a = {"TOM","BOB","ALICE",}
+# b = {"SAM","TOM","BOB","ALICE","GREG"}
+# print(a.issubset(b))
+
+
+# a = {"SAM","TOM","BOB",}
+# b = {"SAM","TOM","BOB","ALICE"}
+# print(a.issuperset(b))
+
+a = {"SAM","TOM","BOB","ALICE"}
+b = {"SAM","TOM","BOB","ALICE"}
+print(a.union(b))
